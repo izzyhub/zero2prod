0e86b6d9551d19cab93e366f80049ff7dd25b21b
Izzy <izzy@izzywatson.com>
2022-03-31 23:39:38 -0400

    Integration test for health_check

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use zero2prod::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let address = "127.0.0.1:8000";
+    let listener = TcpListener::bind(address).expect(&format!("Could not bind {address}"));
+    run(listener)?.await
 }
