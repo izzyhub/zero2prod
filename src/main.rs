44c3b0585eed5289b2d46aef158c92b8322aedfd
Izzy <izzy@izzywatson.com>
2022-04-02 01:49:58 -0400

    Insert a new row on subscription.
    End of chapter 3.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let address = "127.0.0.1:8000";
-    let listener = TcpListener::bind(address).expect(&format!("Could not bind {address}"));
-    run(listener)?.await
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let db_connection_pool = PgPool::connect(&configuration.database.connection_url())
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = TcpListener::bind(address)?;
+    run(listener, db_connection_pool)?.await?;
+    Ok(())
 }
--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,5 +1,6 @@
 use actix_web::{HttpResponse, Responder};
 
 pub async fn health_check() -> impl Responder {
+    println!("Anybody???");
     HttpResponse::Ok()
 }
