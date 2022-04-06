eeb41061b237dae3c09a12fab81f678a01302508
Izzy <izzy@izzywatson.com>
2022-04-05 22:48:09 -0400

    Add basic logging.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,6 @@
 use actix_web::{web, HttpResponse, Responder};
 use chrono::Utc;
+use log;
 use sqlx::PgPool;
 use uuid::Uuid;
 
@@ -10,6 +11,7 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> impl Responder {
+    log::info!("Saving new subscriber details in the database");
     let result = sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -24,7 +26,13 @@ pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) ->
     .await;
 
     match result {
-        Ok(_) => HttpResponse::Ok(),
-        Err(e) => HttpResponse::InternalServerError(),
+        Ok(_) => {
+            log::info!("New subscriber details have been saved");
+            HttpResponse::Ok()
+        }
+        Err(e) => {
+            log::error!("Failed to execute query: {:?}", e);
+            HttpResponse::InternalServerError()
+        }
     }
 }
